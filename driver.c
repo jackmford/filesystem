501920413112d95c3ed23f70233a073a3ce19e22
jackmford <fordjac@bvu.edu>
2019-11-17 14:13:45 -0600

    driver

--- driver.c
+++ driver.c
@@ -5,6 +5,19 @@ int main(int argc, char** argv) {
   const char *f = "test";
   bv_init(f);
   bv_ls();
-  bv_destroy();
-  bv_ls();
+  const char *t = "testingopen";
+  const char *tu = "testingopen";
+  short addr = bv_open(t, 2);
+  printf("Driver %d\n", addr);
+  short writethis[10];
+  writethis[0] = 200;
+  short num = 2;
+  const void *ptr = &num;
+  //bv_ls();
+  //bv_unlink(tu);
+  //bv_unlink(t);
+  //bv_ls();
+  bv_write(addr, ptr, 2);
+//  bv_destroy();
+//  bv_ls();
 }
